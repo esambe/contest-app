c383f11d074747401ce6355f58b14728a4ddab39
Esambe Njume <elvis@makonjo.com>
2020-03-02 10:15:34 +0100

    new 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
     <div class="d-flex justify-content-center align-content-center">
-        <div>
-            <h1 class="text-center tx-white">Sorry the page you are looking for is not available</h1>
+        <div class="container">
+            <h1 class="text-center tx-white">Sorry the page you are looking for is not available!</h1>
             <p class="tx-center">
                 <span class="tx-60 tx-white"><i class="fa fa-frown-o"></i></span>
             </p>
-
+            <div class="tx-center">
+                <a href="{{ url('/') }}" class="text-white tx-30"><u>Back home</u></a>
+            </div>
         </div>
     </div>
 @endsection
